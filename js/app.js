57d616b5fd56d5f9247577f3e662ea9d2ce2de5c
anaseem80 <anaseemamin@gmail.com>
2023-02-10 02:25:52 +0200

    real estate details page

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky + 25) {
+  if (window.pageYOffset >= sticky ) {
     navbar.classList.add("fixed-top")
   } else {
     navbar.classList.remove("fixed-top");
